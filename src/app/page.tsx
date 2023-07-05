1bfb70a7753fa25d7402b56721f300e485aa3d74
Frank <franklaercio@gmail.com>
2023-07-05 18:28:59 -0300

    fix: list of users visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
     "concurseiro",
     "vestibulando",
     "universitário",
-    "coordenador de curso",
-    "secretário acadêmico",
+    "coordenador",
+    "acadêmico",
     "bibliotecário",
   ];
 
